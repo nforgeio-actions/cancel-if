18fac133fd75178a04d621e25f3e00a32b53fed3
Jeff Lill <jeff@lilltek.com>
2021-05-08 14:58:48 -0700

    coded

--- README.md
+++ README.md
@@ -2,4 +2,29 @@
 
 **INTERNAL USE ONLY:** This GitHub action is not intended for general use.  The only reason why this repo is public is because GitHub requires it.
 
-Cancels the workflow on a condition (e.g. the workflow has been queued for too long)
+Cancels the workflow on a condition (e.g. the workflow has been queued for too long).  This works by 
+using the GitHub REST API to cancel the workflow when the condition is satisfied.  Note that the
+action never returns when the workflow is canceled; we rely on the runner to abort the action and
+workflow processes.
+
+Note that this requires access to the current user's master 1Password to retrieve the GITHUB_PAT,
+so you should run the **environment** action first.
+
+## Examples
+
+**Cancel the workflow when it's been queued for more than the 45 minutes:**
+```
+steps:
+
+# We need this so that the master password will be loaded as an environment variable.
+
+- uses: nforgeio-actions/environment
+  with:
+    master-password: ${{ secrets.DEVBOT_MASTER_PASSWORD }}
+    
+# Cancel the workflow when it's been queued for 45 minutes or longer.
+
+- using: nforgeio/cancel-if@master
+  with:
+    queued-minutes-exceeded: 45
+```
--- action.ps1
+++ action.ps1
@@ -30,104 +30,53 @@ Push-Location $ncPowershell
 . ./includes.ps1
 Pop-Location
 
-# Loads an environment variable into the current job environment.
-
-function LoadVariable
+try
 {
-    [CmdletBinding()]
-    param (
-        [Parameter(Position=0, Mandatory=$true)]
-        [string]$variable
-    )
+    # Fetch the inputs
 
-    $value = [System.Environment]::GetEnvironmentVariable($variable)
+    $queuedMinutesExceeded = Get-ActionInput "queued-minutes-exceeded" $false
 
-    if ([System.String]::IsNullOrEmpty($value))
+    if ([System.String]::IsNullOrEmpty($queuedMinutesExceeded))
     {
-        throw "The [$variable] environment variable does not exist."
+        return;     # No constraint to enforce
     }
 
-    Set-ActionOutput $variable $value
-}
+    $maxQueuedMinutes = [System.Int32]::Parse($queuedMinutesExceeded)
+    $minCreatedAtUtc  = [System.DateTime]::UtcNow - [System.TimeSpan]::FromMinutes($maxQueuedMinutes)
 
-# COMPUTERNAME is a special case
+    # Fetch the GITHUB_PAT and query the GitHub REST API for the RunAsAdministrator
+    # information, specifically the [created_at] property.
+    #
+    # The relevant REST APIs are:
+    #
+    #   https://docs.github.com/en/rest/reference/actions#get-a-workflow-run
+    #   https://docs.github.com/en/rest/reference/actions#cancel-a-workflow-run
 
-$computername = [System.Net.Dns]::GetHostName()
-Set-ActionOutput "COMPUTERNAME" $computername
-
-# Load the environment variables
-
-LoadVariable NF_REPOS
-LoadVariable NF_BUILD
-LoadVariable NF_CACHE
-LoadVariable NF_CODEDOC
-LoadVariable NF_ROOT
-LoadVariable NF_SAMPLES_CADENCE
-LoadVariable NF_SNIPPETS
-LoadVariable NF_TEMP
-LoadVariable NF_TEST
-LoadVariable NF_TOOLBIN
-
-LoadVariable NC_ACTIONS_ROOT
-LoadVariable NC_BUILD
-LoadVariable NC_CACHE
-LoadVariable NC_NUGET_DEVFEED
-LoadVariable NC_NUGET_VERSIONER
-LoadVariable NC_REPOS
-LoadVariable NC_ROOT
-LoadVariable NC_TEMP
-LoadVariable NC_TEST
-LoadVariable NC_TOOLBIN
-
-# When the action has access to DEVBOT's master 1Password, persist the password
-# to the MASTER-PASSWORD environment variable and also load useful common 
-# secrets into the process environment and job environments.
-
-$masterPassword = Get-ActionInput "master-password"
-
-if ([System.String]::IsNullOrEmpty($masterPassword))
-{
-    $masterPassword = $env:MASTER_PASSWORD
-}
-
-if (![System.String]::IsNullOrEmpty($masterPassword))
-{
-    [System.Environment]::SetEnvironmentVariable("MASTER_PASSWORD", $masterPassword)
-
-    # Reads a 1Password secret and adds it to the process and
-    # job environments.
-
-    function LoadSecret
-    {
-        [CmdletBinding()]
-        param (
-            [Parameter(Position=0, Mandatory=$true)]
-            [string]$variable,
-            [Parameter(Position=1, Mandatory=$true)]
-            [string]$secretName
-        )
-
-        $value = Get-SecretValue -name $secretName -masterPassword $masterPassword -nullOnNotFound $false
-
-        if (![System.String]::IsNullOrEmpty($value))
-        {
-            [System.Environment]::SetEnvironmentVariable($variable, $value)
-            Set-ActionOutput $variable $value
-        }
+    $GITHUB_PAT = Get-SecretPassword "GITHUB_PAT"
+    $runUri     = "https://api.github.com/repos/$env:GITHUB_REPOSITORY/actions/runs/$env:GITHUB_RUN_ID"
+    $headers    = 
+    @{
+        "authorization" = "Bearer $GITHUB_PAT"
+        "accept"        = "application/vnd.github.v3+json"
     }
 
-    LoadSecret "AWS_ACCESS_KEY_ID"     "AWS_ACCESS_KEY_ID[password]"
-    LoadSecret "AWS_SECRET_ACCESS_KEY" "AWS_SECRET_ACCESS_KEY[password]"
-    LoadSecret "DOCKER_USERNAME"       "DOCKER_LOGIN[username]"
-    LoadSecret "DOCKER_PASSWORD"       "DOCKER_LOGIN[password]"
-    LoadSecret "GITHUB_USERNAME"       "GITHUB_LOGIN[username]"
-    LoadSecret "GITHUB_PASSWORD"       "GITHUB_LOGIN[password]"
-    LoadSecret "GITHUB_PAT"            "GITHUB_PAT[password]"
-    LoadSecret "NEONFORGE_USERNAME"    "NEONFORGE_LOGIN[username]"
-    LoadSecret "NEONFORGE_PASSWORD"    "NEONFORGE_LOGIN[password]"
-    LoadSecret "NUGET_PUBLIC_KEY"      "NUGET_PUBLIC_KEY[password]"
-    LoadSecret "NUGET_VERSIONER_KEY"   "NUGET_VERSIONER_KEY[value]"
-    LoadSecret "NUGET_DEVFEED_KEY"     "NUGET_DEVFEED_KEY[value]"
-    LoadSecret "TEAM_DEVOPS_CHANNEL"   "TEAM_DEVOPS_CHANNEL[value]"
-}
+    $runJson      = $(Invoke-WebRequest -Method GET -Uri $runUri -UserAgent "neonforge.com/0" -Headers $headers)
+    $runDetails   = ConvertFrom-Json $runJson
+    $createdAtUtc = $runDetails.created_at
 
+    if ($createdAtUtc -le $minCreatedAtUtc)
+    {
+        # The run was queued for too long, so cancel it.
+
+        Invoke-WebRequest -Method POST -Uri "$runUri/cancel" -UserAgent "neonforge.com/0" -Headers $headers
+
+        # Sleep long enough for the runner to terminate the workflow.
+
+        Start-Sleep -Seconds 3600
+    }
+}
+catch
+{
+    Write-ActionException $_
+    exit 1
+}
